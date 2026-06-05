e517c197f7745e6c02ea289a700c13de3094c901
Nkosinathiz_ <ayceofunsound@gmail.com>
2026-06-05 12:19:19 +0200

    Storing Data with JSON

---
+++ data.js
@@ -0,0 +1,10 @@
+const book = {
+    title: 'Ego is the enemy',
+    authot: 'Ryan Holiday'
+}
+
+// Convert JavaScript object into JSON 
+const bookJSON = JSON.stringify(book)
+
+const bookObject = JSON.parse(bookJSON)
+console.log(bookObject.title) //Print: Ego is the enemy
