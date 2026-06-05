c2068d2ce0e7a62ad3d8d674a1d8ae0669135e03
Nkosinathiz_ <ayceofunsound@gmail.com>
2026-06-05 12:11:45 +0200

    Argument Parsing with Yargs: Part II

--- app.js
+++ app.js
@@ -1,4 +1,56 @@
-const validator = require('validator')
+// const validator = require('validator')
 
-console.log(validator.isURL('https://www.npmjs.com/package/chalk')) // Print: true
+// console.log(validator.isURL('https://www.npmjs.com/package/chalk')) // Print: true
 
+// const command = process.argv[2]
+
+// if (command === 'add') {
+//     console.log('Adding note!')
+// } else if(command === 'remove'){
+//     console.log('Removing note!')
+// }
+
+// const yargs = require('yargs')
+
+// yargs.version('1.1.0')
+
+// yargs.command({
+//     command: 'add',
+//     describe: 'Add a new note',
+//     handler: function (){
+//         console.log('Adding a new note!')
+//     }
+// })
+
+// console.log(yargs.argv)
+
+// Argument Parsing with Yargs: Part II 
+
+const { title } = require('node:process')
+const { describe } = require('node:test')
+const yargs = require('yargs')
+
+yargs.version('1.1.0')
+
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv){
+        console.log('Title: ' + argv.title)
+        console.log('Body: ' + argv.body)
+    }
+})
+
+console.log(yargs.argv)
